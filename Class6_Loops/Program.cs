015dbd795cbb5eb00671d4beb0a321e450d414ec
IsaMelendezT <157085293+IsaMelendezT@users.noreply.github.com>
2024-02-13 19:29:23 -0500

    Agregar archivos de proyecto.

---
+++ Class6_Loops/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Class6_Loops
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Class Exercise
+            Console.WriteLine("--------------------- Exercise 1 -----------------------");
+            int num = 0;
+            do
+            {
+                Console.WriteLine("Please enter a number below 10: ");
+                num = int.Parse(Console.ReadLine());
+
+            } while (num > 10);
+
+            Console.WriteLine("n = " + num);
+
+            for (int i = 1; i < 10; i++) {
+
+                Console.WriteLine("{0} * {1} = {2}", i, num, i * num);
+            }
+
+            Console.WriteLine("--------------------- Exercise 2 -----------------------");
+
+        }
+    }
+}
